de3f25a4191887bd24462c29e4f923cf80b5cbb8
Denys Zhuravel <denys.zhuravel@albelli.com>
2018-10-09 10:01:44 +0200

    Use shared projects to properly share the code between the two assemblies

---
+++ src/LaunchDarkly.Common.Shared/ClientEnvironment.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+
+namespace LaunchDarkly.Common
+{
+    /// <summary>
+    /// Defines basic properties that differ between the server-side and mobile clients.
+    /// </summary>
+    internal abstract class ClientEnvironment
+    {
+        /// <summary>
+        /// The assembly version string.
+        /// </summary>
+        public string VersionString
+        {
+            get
+            {
+                Type thisType = this.GetType();
+                // Note, this is the type of the concrete subclass, not the base ClientEnvironment.
+                // Therefore we can use it to get information about whichever client assembly it belongs to.
+                var infoAttr = (AssemblyInformationalVersionAttribute)thisType.GetTypeInfo().Assembly
+                    .GetCustomAttribute(typeof(AssemblyInformationalVersionAttribute));
+                return infoAttr.InformationalVersion;
+            }
+        }
+
+        /// <summary>
+        /// The assembly version as a Version object.
+        /// </summary>
+        public Version Version
+        {
+            get
+            {
+                Type thisType = this.GetType();
+                return thisType.GetTypeInfo().Assembly.GetName().Version;
+            }
+        }
+
+        /// <summary>
+        /// The part of the User-Agent header before the slash.
+        /// </summary>
+        public abstract string UserAgentType { get; }
+    }
+}
---
+++ src/LaunchDarkly.Common.Shared/IStreamProcessor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace LaunchDarkly.Common
+{
+    // Interface for platform-specific implementations of the streaming connection,
+    // called from StreamManager.
+    internal interface IStreamProcessor
+    {
+        /// <summary>
+        /// Handle a message from the stream. Implementations of this method should be async.
+        /// </summary>
+        /// <param name="streamManager">the StreamManager instance; this is passed so
+        /// that you can set its Initialized property or call Restart if necessary</param>
+        /// <param name="messageType">the SSE event type</param>
+        /// <param name="messageData">the event data, as a string</param>
+        /// <returns>nothing; implementations should be "async void"</returns>
+        Task HandleMessage(StreamManager streamManager, string messageType, string messageData);
+    }
+}
