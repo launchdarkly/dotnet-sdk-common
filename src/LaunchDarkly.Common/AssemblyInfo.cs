13425da9d13c32bbca81119766e790f6a785aa4e
Eli Bishop <eli@launchdarkly.com>
2018-10-25 16:55:40 -0700

    prepare 1.2.0 release (#13)

--- src/LaunchDarkly.Common/AssemblyInfo.cs
+++ src/LaunchDarkly.Common/AssemblyInfo.cs
@@ -4,5 +4,9 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("LaunchDarkly.Xamarin")]
 [assembly: InternalsVisibleTo("LaunchDarkly.Xamarin.Tests")]
 
+// Allow *unsigned* version of LD.Client to be used with unsigned LD.Common; currently there
+// is no such build of the client, but someone forking the library might do this
+[assembly: InternalsVisibleTo("LaunchDarkly.Client")]
+
 // Allow mock/proxy objects in unit tests to access internal classes
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
