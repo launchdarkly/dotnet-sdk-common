2581e1f47a8afa6fdadf6e3caa1f9286fc6721ff
Eli Bishop <eli@launchdarkly.com>
2018-12-12 21:29:53 -0800

    comment

--- scripts/release.ps1
+++ scripts/release.ps1
@@ -1,3 +1,15 @@
+#
+# Simple PowerShell script for building and uploading NuGet packages from dotnet-client-common.
+#
+# Both LaunchDarkly.Common and LaunchDarkly.Common.StrongName will be published, in the Release
+# configuration (after first building and testing in Debug configuration to make sure it works).
+#
+# Before you run this script, make sure:
+# 1. you have set the correct project version in both LaunchDarkly.Common.csproj and
+#    LaunchDarkly.Common.StrongName.csproj
+# 2. you have downloaded the key file, LaunchDarkly.Common.snk, in the project root directory
+#
+
 dotnet clean
 dotnet build -c Debug
 dotnet test -c Debug test\LaunchDarkly.Common.Tests\LaunchDarkly.Common.Tests.csproj
