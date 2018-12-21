c0a99406cd1f122e09cb0b4aff78c29becadfca8
Eli Bishop <eli@launchdarkly.com>
2018-12-21 14:26:27 -0800

    better controls and output

--- scripts/release.ps1
+++ scripts/release.ps1
@@ -1,4 +1,9 @@
-param([string]$password = $(throw "-password is required"))
+param(
+    [string]$password = "",
+    [switch]$skipPublish = $false,
+    [switch]$skipSign = $false,
+    [switch]$skipTests = $false
+)
 
 $assemblyNames = "LaunchDarkly.Common", "LaunchDarkly.Common.StrongName"
 $testProject = "LaunchDarkly.Common.Tests"
@@ -18,6 +23,8 @@ $testProject = "LaunchDarkly.Common.Tests"
 # 4. you have downloaded the certificate, catamorphic_code_signing_certificate.p12, in the
 #    project root directory
 # 5. you have the password for the certificate (paste it after -password on the command line)_
+#
+# To test the build and packaging without uploading to NuGet, add -skipPublish.
 #
 
 # This helper function comes from https://github.com/psake/psake - it allows us to terminate the
@@ -41,20 +48,45 @@ if (-not (Test-Path $certFile)) {
     throw ("Certificate file $certfile must be in the current directory. Download it from s3://launchdarkly-pastebin/ci/dotnet/")
 }
 
+if ((-not $skipSign) -and ($password -eq "")) {
+    throw "-password is required unless you specify -skipSign"
+}
+
+if ((-not $skipPublish) -and $skipSign) {
+    throw "cannot publish without signing"
+}
+
 Exec { dotnet clean }
 
-Exec { dotnet build -c Debug }
-Exec { dotnet test -c Debug test\$testProject }
+if ($skipTests) {
+    Write-Host "Skipping debug build and unit tests" -ForegroundColor black -BackgroundColor yellow
+} else {
+    Exec { dotnet build -c Debug }
+    Exec { dotnet test -c Debug test\$testProject }
+}
 
 foreach ($assemblyName in $assemblyNames) {
     Exec { dotnet build -c Release "src\$assemblyName" }
+
     $dlls = dir -Path "src\$assemblyName\bin\Release" -Filter "$assemblyName.dll" -Recurse | %{$_.FullName}
-    Exec { signtool sign /f $certFile /p $password $dlls }
+    if ($skipSign) {
+        foreach ($dll in $dlls) {
+            Write-Host "Skipping signing of: $dll" -ForegroundColor black -BackgroundColor yellow
+        }
+    } else {
+        Exec { signtool sign /f $certFile /p $password $dlls }
+    }
 
     del "src\$assemblyName\bin\Release\*.nupkg"
     Exec { dotnet pack -c Release "src\$assemblyName" }
 }
 
 foreach ($assemblyName in $assemblyNames) {
-    dotnet nuget push "src\$assemblyName\bin\Release\*.nupkg" -s https://www.nuget.org
+    $pkg = dir -Path "src\$assemblyName\bin\Release\*.nupkg" | %{$_.FullName}
+    if ($skipPublish) {
+        Write-Host "Skipping publishing of: $pkg" -ForegroundColor black -BackgroundColor yellow
+    } else {
+        Exec { dotnet nuget push $pkg -s https://www.nuget.org }
+        Write-Host "Published: $pkg" -ForegroundColor black -BackgroundColor green
+    }
 }
