6773c5d5469cd095123f24838e844e38d4df365b
Eli Bishop <eli@launchdarkly.com>
2018-12-20 19:39:53 -0800

    sign assemblies in release script

--- scripts/release.ps1
+++ scripts/release.ps1
@@ -1,3 +1,8 @@
+param([string]$password = $(throw "-password is required"))
+
+$assemblyNames = "LaunchDarkly.Common", "LaunchDarkly.Common.StrongName"
+$testProject = "LaunchDarkly.Common.Tests"
+
 #
 # Simple PowerShell script for building and uploading NuGet packages from dotnet-client-common.
 #
@@ -10,12 +15,44 @@
 # 2. you have downloaded the key file, LaunchDarkly.Common.snk, in the project root directory
 #
 
-dotnet clean
-dotnet build -c Debug
-dotnet test -c Debug test\LaunchDarkly.Common.Tests\LaunchDarkly.Common.Tests.csproj
-dotnet build -c Release
-del src\LaunchDarkly.Common\bin\Release\*.nupkg
-del src\LaunchDarkly.Common.StrongName\bin\Release\*.nupkg
-dotnet pack -c Release
-dotnet nuget push src\LaunchDarkly.Common\bin\Release\*.nupkg -s https://www.nuget.org
-dotnet nuget push src\LaunchDarkly.Common.StrongName\bin\Release\*.nupkg -s https://www.nuget.org
+# This helper function comes from https://github.com/psake/psake - it allows us to terminate the
+# script if any executable returns an error code, which PowerShell won't otherwise do
+function Exec
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0,Mandatory=1)][scriptblock]$cmd,
+        [Parameter(Position=1,Mandatory=0)][string]$errorMessage = ("Error executing command {0}" -f $cmd)
+    )
+    & $cmd
+    if ($lastexitcode -ne 0) {
+        throw ($errorMessage)
+    }
+}
+
+$certFile = "catamorphic_code_signing_certificate.p12"
+
+if (-not (Test-Path $certFile)) {
+    throw ("Certificate file $certfile must be in the current directory. Download it from s3://launchdarkly-pastebin/ci/dotnet/")
+}
+
+$env:Path += ";C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Bin"
+
+Exec { dotnet clean }
+
+#Exec { dotnet build -c Debug }
+#Exec { dotnet test -c Debug test\$testProject }
+
+Exec { dotnet build -c Release }
+
+foreach ($assemblyName in $assemblyNames) {
+    $dlls = dir -Path "src\$assemblyName\bin\Release" -Filter "$assemblyName.dll" -Recurse | %{$_.FullName}
+    Exec { signtool sign /f $certFile /p $password $dlls }
+
+    del "src\$assemblyName\bin\Release\*.nupkg"
+    Exec { dotnet pack -c Release "src\$assemblyName" }
+}
+
+foreach ($assemblyName in $assemblyNames) {
+    dotnet nuget push "src\$assemblyName\bin\Release\*.nupkg" -s https://www.nuget.org
+}
