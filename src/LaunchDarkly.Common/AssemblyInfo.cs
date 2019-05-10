b2fca64ae142064bb803399a6e19aec52b3b8a89
Eli Bishop <eli@launchdarkly.com>
2019-05-10 12:56:15 -0700

    prepare 2.1.1 release of common package with new package names

--- src/LaunchDarkly.Common/AssemblyInfo.cs
+++ src/LaunchDarkly.Common/AssemblyInfo.cs
@@ -1,13 +1,13 @@
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("LaunchDarkly.Common.Tests")]
-[assembly: InternalsVisibleTo("LaunchDarkly.Xamarin")]
-[assembly: InternalsVisibleTo("LaunchDarkly.Xamarin.Tests")]
+[assembly: InternalsVisibleTo("LaunchDarkly.CommonSdk.Tests")]
+[assembly: InternalsVisibleTo("LaunchDarkly.XamarinSdk")]
+[assembly: InternalsVisibleTo("LaunchDarkly.XamarinSdk.Tests")]
 
-// Allow *unsigned* version of LD.Client to be used with unsigned LD.Common; currently there
+// Allow *unsigned* version of LD.ServerSdk to be used with unsigned LD.CommonSdk; currently there
 // is no such build of the client, but someone forking the library might do this
-[assembly: InternalsVisibleTo("LaunchDarkly.Client")]
-[assembly: InternalsVisibleTo("LaunchDarkly.Tests")]
+[assembly: InternalsVisibleTo("LaunchDarkly.ServerSdk")]
+[assembly: InternalsVisibleTo("LaunchDarkly.ServerSdk.Tests")]
 
 // Allow mock/proxy objects in unit tests to access internal classes
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
