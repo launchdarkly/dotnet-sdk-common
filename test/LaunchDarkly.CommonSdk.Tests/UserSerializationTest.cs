34f59c3cc495608e8c28c6313fddab945a57fe32
Eli Bishop <eli@launchdarkly.com>
2019-07-23 16:04:24 -0700

    prepare 2.2.0 release (#23)

---
+++ test/LaunchDarkly.CommonSdk.Tests/UserSerializationTest.cs
@@ -0,0 +1,66 @@
+﻿using LaunchDarkly.Client;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace LaunchDarkly.Common.Tests
+{
+    public class UserSerializationTests : UserBuilderTestBase
+    {
+        private const string key = "UserKey";
+
+        // Note that users are never deserialized from JSON in the .NET SDK, but they are in the Xamarin SDK.
+
+        [Fact]
+        public void DeserializeBasicUserAsJson()
+        {
+            var json = $"{{\"key\":\"{key}\"}}";
+            var user = JsonConvert.DeserializeObject<User>(json);
+            Assert.Equal(key, user.Key);
+        }
+
+        [Fact]
+        public void DeserializeUserWithCustomAsJson()
+        {
+            var json = $"{{\"key\":\"{key}\", \"custom\": {{\"a\":\"b\"}}}}";
+            var user = JsonConvert.DeserializeObject<User>(json);
+            Assert.Equal("b", (string)user.Custom["a"]);
+        }
+
+        [Fact]
+        public void SerializingAndDeserializingAUserWithCustomAttributesIsIdempotent()
+        {
+            var user = User.Builder(key).Custom("a", "b").Build();
+            var json = JsonConvert.SerializeObject(user);
+            var newUser = JsonConvert.DeserializeObject<User>(json);
+            Assert.Equal("b", (string)user.Custom["a"]);
+            Assert.Equal(key, user.Key);
+        }
+
+        [Fact]
+        public void SerializingAUserWithNoAnonymousSetYieldsNoAnonymous()
+        {
+            var user = User.WithKey(key);
+            var json = JObject.FromObject(user);
+            Assert.Null(json["anonymous"]);
+        }
+
+        [Fact]
+        public void SerializingAUserWithAnonymousSetYieldsAnonymousTrue()
+        {
+            var user = User.Builder(key).Anonymous(true).Build();
+            var json = JObject.FromObject(user);
+            Assert.Equal(new JValue(true), json["anonymous"]);
+        }
+
+        [Theory]
+        [MemberData(nameof(AllStringProperties))]
+        public void CanSerializeStringProperty(StringPropertyDesc p)
+        {
+            var value = "x";
+            var user = p.Setter(User.Builder(key))(value).Build();
+            var json = JObject.FromObject(user);
+            Assert.Equal(new JValue(value), json[p.Name]);
+        }
+    }
+}
---
+++ test/LaunchDarkly.CommonSdk.Tests/UserTest.cs
@@ -0,0 +1,85 @@
+﻿using LaunchDarkly.Client;
+using Xunit;
+
+namespace LaunchDarkly.Common.Tests
+{
+    public class UserTest
+    {
+        private const string key = "UserKey";
+        
+        public static readonly User UserToCopy = User.Builder("userkey")
+                .SecondaryKey("s")
+                .IPAddress("1")
+                .Country("US")
+                .FirstName("f")
+                .LastName("l")
+                .Name("n")
+                .Avatar("a")
+                .Email("e")
+                .Custom("c1", "v1")
+                .Custom("c2", "v2").AsPrivateAttribute()
+                .Build();
+
+        [Fact]
+        public void UserWithKeySetsKey()
+        {
+            var user = User.WithKey(key);
+            Assert.Equal(key, user.Key);
+        }
+        
+        [Fact]
+        public void TestPropertyDefaults()
+        {
+            var user = User.WithKey(key);
+            Assert.Null(user.SecondaryKey);
+            Assert.Null(user.IPAddress);
+            Assert.Null(user.Country);
+            Assert.Null(user.FirstName);
+            Assert.Null(user.LastName);
+            Assert.Null(user.Name);
+            Assert.Null(user.Avatar);
+            Assert.Null(user.Email);
+            Assert.NotNull(user.Custom);
+            Assert.Equal(0, user.Custom.Count);
+            Assert.Null(user.PrivateAttributeNames);
+        }
+
+        [Fact]
+        public void SettingDeprecatedIpSetsIP()
+        {
+            var ip = "1.2.3.4";
+            var user = User.WithKey(key);
+#pragma warning disable 618
+            user.IpAddress = ip;
+#pragma warning restore 618
+            Assert.Equal(ip, user.IPAddress);
+        }
+
+        [Fact]
+        public void GettingDeprecatedIpGetsIP()
+        {
+            var ip = "1.2.3.4";
+            var user = User.WithKey(key);
+            user.IPAddress = ip;
+#pragma warning disable 618
+            Assert.Equal(ip, user.IpAddress);
+#pragma warning restore 618
+        }
+
+        [Fact]
+        public void TestUserSelfEquality()
+        {
+            Assert.True(UserToCopy.Equals(UserToCopy));
+        }
+        
+        [Fact]
+        public void TestUserEqualityWithCopyConstructor()
+        {
+            User copy = new User(UserToCopy);
+            Assert.NotSame(UserToCopy, copy);
+            Assert.True(copy.Equals(UserToCopy));
+            Assert.True(UserToCopy.Equals(copy));
+            Assert.Equal(UserToCopy.GetHashCode(), copy.GetHashCode());
+        }
+    }
+}
