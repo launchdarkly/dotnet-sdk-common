ec623c8184d72ae28d29232c636c016c7a3078a7
Eli Bishop <eli@launchdarkly.com>
2019-08-03 13:37:06 -0700

    misc cleanup of common types

--- src/LaunchDarkly.CommonSdk/Internal/StreamProperties.cs
+++ src/LaunchDarkly.CommonSdk/Internal/StreamProperties.cs
@@ -4,7 +4,7 @@ using System.Net.Http;
 namespace LaunchDarkly.Common
 {
     // Used by StreamManager to specify how to make a streaming connection.
-    internal class StreamProperties
+    internal struct StreamProperties
     {
         /// <summary>
         /// The URI for the streaming connection. This is normally constructed by adding some
@@ -13,19 +13,19 @@ namespace LaunchDarkly.Common
         /// stream URI than before, you should dispose of the current StreamManager and
         /// IStreamProcessor and create new ones.
         /// </summary>
-        public Uri StreamUri { get; private set;  }
+        public Uri StreamUri { get; }
 
         /// <summary>
         /// The HTTP method to use for the streaming connection.
         /// NOTE: Currently only GET is supported.
         /// </summary>
-        public HttpMethod Method { get; private set; }
+        public HttpMethod Method { get; }
 
         /// <summary>
         /// The request body to send if the method is not GET; null for no body.
         /// NOTE: Currently not used, since only GET is supported.
         /// </summary>
-        public HttpContent RequestBody { get; private set; }
+        public HttpContent RequestBody { get; }
 
         public StreamProperties(Uri uri, HttpMethod method, HttpContent body)
         {
