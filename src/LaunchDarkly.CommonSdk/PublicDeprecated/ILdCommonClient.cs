4d96088c01d551bab6c690dc20ab34ef3375b646
Eli Bishop <eli@launchdarkly.com>
2019-08-30 23:35:48 -0700

    copy the better doc comments that were in LdClient

--- src/LaunchDarkly.CommonSdk/PublicDeprecated/ILdCommonClient.cs
+++ src/LaunchDarkly.CommonSdk/PublicDeprecated/ILdCommonClient.cs
@@ -13,18 +13,32 @@ namespace LaunchDarkly.Common
     public interface ILdCommonClient : IDisposable
     {
         /// <summary>
-        /// Tests whether the client is being used in offline mode.
+        /// Tests whether the client is configured to be offline.
         /// </summary>
-        /// <returns>true if the client is offline</returns>
+        /// <returns>true if using an offline configuration</returns>
         bool IsOffline();
 
         /// <summary>
-        /// Flushes all pending events.
+        /// Tells the client that all pending analytics events should be delivered as soon as possible.
         /// </summary>
+        /// <remarks>
+        /// <para>
+        /// When the LaunchDarkly client generates analytics events (from flag evaluations, or from
+        /// the <c>Identify</c> or <c>Track</c> methods), they are queued on a worker thread. The event
+        /// thread normally sends all queued events to LaunchDarkly at regular intervals, controlled by
+        /// the <see cref="IBaseConfiguration.EventFlushInterval"/> option. Calling <see cref="Flush"/>
+        /// triggers a send without waiting for the next interval.
+        /// </para>
+        /// <para>
+        /// Flushing is asynchronous, so this method will return before it is complete. However, if you
+        /// shut down the client with <see cref="IDisposable.Dispose()"/>, events are guaranteed to be
+        /// sent before that method returns.
+        /// </para>
+        /// </remarks>
         void Flush();
 
         /// <summary>
-        /// Returns the current version number of the LaunchDarkly client.
+        /// The current version string of the SDK.
         /// </summary>
         Version Version { get; }
     }
