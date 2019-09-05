8509ef0d4a77f2d36b2d95903063d1f3f9de1bc9
Eli Bishop <eli@launchdarkly.com>
2019-09-05 08:49:26 -0700

    comment fixes

--- src/LaunchDarkly.CommonSdk/Internal/ValueType.cs
+++ src/LaunchDarkly.CommonSdk/Internal/ValueType.cs
@@ -11,25 +11,33 @@ namespace LaunchDarkly.Common
 
     /// <summary>
     /// A type-safe standardized mechanism for converting between JSON and all of the value types
-    /// supported by LaunchDarkly SDKs. Use the predefined <see cref="ValueTypes"/> instances.
+    /// supported by LaunchDarkly SDKs. Use the predefined <see cref="ValueTypes"/> instances. Unlike
+    /// the type conversions in <see cref="ImmutableJsonValue"/> which never throw exceptions, these
+    /// converters will throw a <see cref="ValueTypeException"/> if an incompatible type is requested,
+    /// because the SDK evaluation methods need to be able to detect this condition.
     /// </summary>
     /// <remarks>
+    /// <para>
     /// This allows the SDK <c>Variation</c> methods to be implemented more simply and without casting.
-    /// 
+    /// </para>
+    /// <para>
     /// All of these types require an exact match with the JSON type except as follows:
-    /// 
+    /// </para>
+    /// <para>
     /// 1. <c>Json</c> allows any type.
-    /// 
+    /// </para>
+    /// <para>
     /// 2. <c>Int</c> and <c>Float</c> are transparently convertible to each other (this is necessary
-    /// because JSON only really has one numeric type). Note that conversion from float to int uses
-    /// the behavior defined by Newtonsoft.Json, which rounds to the nearest integer rather than
-    /// rounding down. This behavior is preserved for backward compatibility with the .NET SDK.
-    /// 
+    /// because JSON only really has one numeric type).
+    /// </para>
+    /// <para>
     /// 3. <c>String</c> and <c>Json</c> can be converted from either an actual <see langword="null"/> or a
     /// <see cref="JToken"/> of type <see cref="JTokenType.Null"/>.
-    /// 
+    /// </para>
+    /// <para>
     /// This is a struct rather than a class so that in any context where we expect a <c>ValueType</c>,
     /// we do not have to worry about it being null.
+    /// </para>
     /// </remarks>
     /// <typeparam name="T">the desired type</typeparam>
     internal struct ValueType<T>
@@ -38,12 +46,15 @@ namespace LaunchDarkly.Common
         /// Function for converting a JSON value to the desired type.
         /// </summary>
         /// <remarks>
+        /// <para>
         /// If the JSON value is not of a compatible type, this function will throw a
         /// <see cref="ValueTypeException"/>.
-        /// 
+        /// </para>
+        /// <para>
         /// If we ever drop compatibility with older .NET frameworks that do not support <c>ValueTuple</c>,
         /// then it would be preferable to use <c>ValueTuple</c> to avoid the overhead of exceptions.
         /// However, these exceptions should not happen often.
+        /// </para>
         /// </remarks>
         public Func<ImmutableJsonValue, T> ValueFromJson { get; }
 
@@ -73,9 +84,10 @@ namespace LaunchDarkly.Common
             value => ImmutableJsonValue.Of(value)
         );
 
-        // Note that Int rounds floating-point values toward zero (truncate), rather than
-        // using Newtonsoft.Json's default behavior (round to nearest). That is inconsistent
-        // with .NET SDK 2.x, but consistent with most or all of our other strongly-typed SDKs.
+        // Note that Int uses ImmutableJsonValue's rounding behavior for floating-point values
+        // (round toward zero), rather than using Newtonsoft.Json's default behavior (round to
+        // nearest). That is inconsistent with .NET SDK 2.x, but consistent with most or all of
+        // our other strongly-typed SDKs.
         public static readonly ValueType<int> Int = new ValueType<int>(
             json => json.IsNumber ? json.AsInt : throw new ValueTypeException(),
             value => ImmutableJsonValue.Of(value)
