b76add926cbda2771ec4680e754e77f74510309e
Eli Bishop <eli@launchdarkly.com>
2019-09-08 16:59:00 -0700

    fix comment

--- src/LaunchDarkly.CommonSdk/Public/User.cs
+++ src/LaunchDarkly.CommonSdk/Public/User.cs
@@ -220,7 +220,7 @@ namespace LaunchDarkly.Client
         [JsonIgnore]
         public ISet<string> PrivateAttributeNames { get; set; }
 
-        [Obsolete("This method has been moved to the Clause class in .NET, and is not used in Xamarin")]
+        [Obsolete("This method has been moved to the Operator class in .NET, and is not used in Xamarin")]
         internal JToken GetValueForEvaluation(string attribute)
         {
             switch (attribute)
