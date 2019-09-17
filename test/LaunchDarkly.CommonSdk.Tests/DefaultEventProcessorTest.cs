46c1ca855af9741abd0fdcb484f743d0bee31cb2
Gavin Whelan <gwhelan@launchdarkly.com>
2019-09-17 17:54:16 +0000

    Set the flush TimeSpant for DefaultEventProcessorTest to -1 milliseconds instead of 1 minute. This prevents the flush timer from starting. We always trigger a flush manually in these tests, so this seems reasonable.

--- test/LaunchDarkly.CommonSdk.Tests/DefaultEventProcessorTest.cs
+++ test/LaunchDarkly.CommonSdk.Tests/DefaultEventProcessorTest.cs
@@ -27,7 +27,7 @@ namespace LaunchDarkly.Common.Tests
         {
             _server = FluentMockServer.Start();
             _config.EventsUri = new Uri(_server.Urls[0]);
-            _config.EventFlushInterval = TimeSpan.FromMinutes(1);
+            _config.EventFlushInterval = TimeSpan.FromMilliseconds(-1);
         }
 
         void IDisposable.Dispose()
