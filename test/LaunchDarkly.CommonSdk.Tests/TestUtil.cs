c751532594e79ed4b937536273ee9572efe24985
Eli Bishop <eli@launchdarkly.com>
2019-11-09 12:05:28 -0800

    fix summary event "unknown" logic, improve tests

--- test/LaunchDarkly.CommonSdk.Tests/TestUtil.cs
+++ test/LaunchDarkly.CommonSdk.Tests/TestUtil.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Linq;
 using Xunit;
 
 namespace LaunchDarkly.Common.Tests
@@ -9,6 +11,15 @@ namespace LaunchDarkly.Common.Tests
         {
             Assert.True(JToken.DeepEquals(expected, actual), actual is null ? "null" : actual.ToString() +
                 " should be " + expected is null ? "null" : expected.ToString());
+        }
+        
+        public static void AssertContainsInAnyOrder<T>(IEnumerable<T> items, params T[] expectedItems)
+        {
+            Assert.Equal(expectedItems.Length, items.Count());
+            foreach (var e in expectedItems)
+            {
+                Assert.Contains(e, items);
+            }
         }
     }
 }
